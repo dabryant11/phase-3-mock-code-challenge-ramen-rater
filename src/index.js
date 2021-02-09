8066b76fca5c3af791fd79138d9940865ecfb301
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-02-09 10:35:08 -0500

    morning1035

--- src/index.js
+++ src/index.js
@@ -1,34 +1,24 @@
-// write your code here
-function getRamens(){ 
-    const url = 'http://localhost:3000/ramens'
-    fetch(url)
-    .then(res => res.json())
-    .then(data => {
-
-        data.forEach(renderRamens)
-    })
-}
-
-function renderRamens = ramen => {
+const renderRamens = ramen => {
     let ramenMenu = document.querySelector('#ramen-menu')
     let newDiv = document.createElement('div')
     // let 
-    // newDiv.className = 'card'
-    // newDiv.dataset.id = ramen.id
+    newDiv.className = 'card'
+    newDiv.dataset.id = ramen.id
 
     newDiv.innerHTML = `
     <img class="detail-image" src=${ramen.image} alt=${ramen.name} />
-    <h2 class="name">${ramen.name}</h2>
-    <h3 class="restaurant">${ramen.restaurant}</h3>
     `
     ramenMenu.append(newDiv)
 }
 
-
-
-
-function displayAllRamens(ramens){
-    ramens.forEach(ramen => {
-        renderRamens(ramen)
+// write your code here
+window.onload = function () { 
+    const url = 'http://localhost:3000/ramens'
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+// console.log(data)
+        data.forEach(renderRamens)
     })
 }
+
