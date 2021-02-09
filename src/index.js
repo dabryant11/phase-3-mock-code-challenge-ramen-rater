d9b1279372a063d9e6635c26e73a02ce5f6a21ed
dabryant11 <74263550+dabryant11@users.noreply.github.com>
2021-02-09 11:44:36 -0500

    nonononononno

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+const ramenMenu = document.querySelector('#ramen-menu')
+
+ramenMenu.addEventListener('click', handleClick)
+
+function handleClick(e){
+    if (e.target.className === 'detail-image'){
+        displayRamenInfo(e)
+    }
+}
+
+function displayRamenInfo(e){
+    const ramenId = e.path[0].id
+    const ramenImage = e.path[0].src
+    const ramenImageName = e.path[0].alt
+    const ramenRestaurant = e.path[2].firstElementChild.innerText
+
+
+
+    let index = ramenId
+    const ramenDetail = document.querySelector('#ramen-detail')
+    console.log(e)
+    ramenDetail.innerHTML = `
+     <img class="detail-image" src=${ramenImage} alt=${ramenImageName} />
+     <h2 class="name">${ramenImageName}</h2>   
+     <h3 class="restaurant">${ramenRestaurant}</h3>  
+   `
+}
+
+
+
+
 const renderRamens = ramen => {
     let ramenMenu = document.querySelector('#ramen-menu')
     let newDiv = document.createElement('div')
@@ -6,9 +37,17 @@ const renderRamens = ramen => {
     newDiv.dataset.id = ramen.id
 
     newDiv.innerHTML = `
-    <img class="detail-image" src=${ramen.image} alt=${ramen.name} />
+    <img class="detail-image" src=${ramen.image} alt=${ramen.name} id=${ramen.id} />
+    <p>${ramen.restaurant}</p>
     `
     ramenMenu.append(newDiv)
+
+    // let detailImage = document.querySelector('.detail-image')
+    // detailImage.addEventListener('click', showDetails)
+
+    // function showDetails(){
+    //     console.log('yoooooo')
+    // }
 }
 
 // write your code here
